d27fd269816bf5a0df3488a9954c2cc542774d15
aaharbaugh <aaronmharbaugh@gmail.com>
2019-09-20 10:14:01 -0500

    wrote ring_buffer, and it passes test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    #add to buffer
+    self.storage[self.current] = item
+
+    if self.current == self.capacity -1:
+      self.current = 0
+    else: 
+      self.current += 1
 
   def get(self):
-    pass
+    #returns buffer storage, without [None]
+    return [ea for ea in self.storage if ea is not None]
